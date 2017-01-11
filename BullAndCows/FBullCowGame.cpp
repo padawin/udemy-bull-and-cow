257b1152ddb72fe80b789b8c0c8df3b8345c475b
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-11 13:26:25 +0000

    welcome used from the game

--- BullAndCows/FBullCowGame.cpp
+++ BullAndCows/FBullCowGame.cpp
@@ -1,11 +1,19 @@
 #include "FBullCowGame.hpp"
+#include <iostream>
 
 FBullCowGame::FBullCowGame() {
 	reset();
 }
 
 void FBullCowGame::welcome() {
-
+	std::cout << "Welcome to Bulls and Cows" << std::endl;
+	std::cout << "I am thinking of a word of " << m_iLengthWord << " letters." << std::endl;
+	std::cout << "You have " << m_iMaxTries << " turns to find it." << std::endl;
+	std::cout << "At each turn, give me a letter and a position (from 1 to " << m_iLengthWord << ") and I will tell you:" << std::endl;
+	std::cout << "- BULL if the letter and the position are correct," << std::endl;
+	std::cout << "- COW if the letter is correct but not the position," << std::endl;
+	std::cout << "- SNAP if neither the letter not the position are correct." << std::endl;
+	std::cout << std::endl;
 }
 
 void FBullCowGame::reset() {
--- BullAndCows/main.cpp
+++ BullAndCows/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include "FBullCowGame.hpp"
 
-void welcome();
 std::string generateWord();
 void readString(std::string &output, const char* prompt);
 void printGuessResult(std::string playerGuessChar);
@@ -12,17 +11,6 @@ bool askIfContinue();
 
 constexpr unsigned int WORLD_LENGTH = 7;
 constexpr unsigned int MAX_TURNS = 15;
-
-void welcome() {
-	std::cout << "Welcome to Bulls and Cows" << std::endl;
-	std::cout << "I am thinking of a word of " << WORLD_LENGTH << " letters." << std::endl;
-	std::cout << "You have " << MAX_TURNS << " turns to find it." << std::endl;
-	std::cout << "At each turn, give me a letter and a position (from 1 to " << WORLD_LENGTH << ") and I will tell you:" << std::endl;
-	std::cout << "- BULL if the letter and the position are correct," << std::endl;
-	std::cout << "- COW if the letter is correct but not the position," << std::endl;
-	std::cout << "- SNAP if neither the letter not the position are correct." << std::endl;
-	std::cout << std::endl;
-}
 
 void readString(std::string &output, const char* prompt) {
 	std::cout << prompt;
@@ -54,7 +42,8 @@ void play(std::string wordToFind) {
 
 int main() {
 	FBullCowGame game;
-	welcome();
+	game.reset();
+	game.welcome();
 	bool continuePlaying = true;
 	do {
 		std::string wordToFind = generateWord();
