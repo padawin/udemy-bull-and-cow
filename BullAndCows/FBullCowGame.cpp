3b061e3b90ff4f87081943d37d99e4032506385c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-01-22 18:35:37 +0000

    welcome updated

--- BullAndCows/FBullCowGame.cpp
+++ BullAndCows/FBullCowGame.cpp
@@ -26,10 +26,9 @@ void FBullCowGame::_welcome() const {
 	std::cout << "Welcome to Bulls and Cows" << std::endl;
 	std::cout << "I am thinking of a word of " << m_iLengthWord << " letters." << std::endl;
 	std::cout << "You have " << m_iMaxTries << " turns to find it." << std::endl;
-	std::cout << "At each turn, give me a letter and a position (from 1 to " << m_iLengthWord << ") and I will tell you:" << std::endl;
-	std::cout << "- BULL if the letter and the position are correct," << std::endl;
-	std::cout << "- COW if the letter is correct but not the position," << std::endl;
-	std::cout << "- SNAP if neither the letter not the position are correct." << std::endl;
+	std::cout << "At each turn, give me a word and I will tell you:" << std::endl;
+	std::cout << "- How many bulls you found (correct letter at the correct place," << std::endl;
+	std::cout << "- How many cows you found (correct letter but badly placed)." << std::endl;
 	std::cout << std::endl;
 }
 
